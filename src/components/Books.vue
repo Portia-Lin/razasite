9530d37b19c33689ccf7ba6ba52911f9491e93ac
escsun <escsunarcher@gmail.com>
2018-11-11 02:34:35 +0200

    Добавил компонент Books

---
+++ src/components/Books.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Book :book="book" :key="index" v-for="(book, index) of books"/>
+  </div>
+</template>
+
+<script>
+  import Book from './Book'
+  export default {
+    name: 'Books',
+    components: { Book },
+    props: ['books']
+  }
+</script>
+
+<style scoped>
+
+</style>
