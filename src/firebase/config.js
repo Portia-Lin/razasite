f2b7b7f906600ad814d35200a2aa091ba30bf0cd
escsun <escsunarcher@gmail.com>
2018-11-11 02:33:50 +0200

    Сделал конфигурацию firebase и подключил ее в main

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyCpqtJpIm4su8ndPcARiNZmI0lXtslXdeU',
+  authDomain: 'razasite-escsun.firebaseapp.com',
+  databaseURL: 'https://razasite-escsun.firebaseio.com',
+  projectId: 'razasite-escsun',
+  storageBucket: 'razasite-escsun.appspot.com',
+  messagingSenderId: '289972343306'
+}
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueFire from 'vuefire'
 import App from './App.vue'
 import router from './router'
 // Подключаем bootsrap
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
-
 Vue.use(BootstrapVue)
+Vue.use(VueFire)
+Vue.config.productionTip = false
 
 new Vue({
   router,
