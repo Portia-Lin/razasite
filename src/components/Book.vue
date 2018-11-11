4735a3bb691b8bdbb34713c2a0138c8dcb0b65ac
escsun <escsunarcher@gmail.com>
2018-11-11 02:34:24 +0200

    Добавил компонент Book

---
+++ src/components/Book.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img class="img" :src="book.image" :alt="book.title">
+    <h1 class="title">{{ book.title }}</h1>
+    <h2 class="author">{{ book.author }}</h2>
+    <p class="genre">{{ book.genre }}</p>
+    <p class="annotation">{{ book.annotation }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Book',
+    props: ['book']
+  }
+</script>
+
+<style scoped>
+  .title {
+    text-transform: uppercase;
+  }
+  .annotation {
+    color: brown;
+  }
+</style>
