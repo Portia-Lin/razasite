1574a3558487b0bfc9cf380e336df596c295643d
escsun <escsunarcher@gmail.com>
2018-11-11 02:35:00 +0200

    Обновил страничку Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="home">
-    <h1>Домашняя страница</h1>
+    <h1 class="center">Домашняя страница</h1>
+    <Books :books="books"/>
   </div>
 </template>
 
 <script>
+  // импортируем базу данных из файла firebase.js
+  import { db } from '../firebase/db'
+  import Books from '../components/Books'
 
-export default {
-  name: 'home',
-  components: {}
-}
+  export default {
+    name: 'home',
+    data () {
+      return {
+        books: []
+      }
+    },
+    firebase: {
+      books: db.ref('books')
+    },
+    components: { Books }
+  }
 </script>
+
+<style scoped>
+  .center {
+    text-align: center;
+  }
+</style>
