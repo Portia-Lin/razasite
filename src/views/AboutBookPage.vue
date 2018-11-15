c3df80523ad36172b8e50fb7e669b4f455870f3c
Portia Lin <portialin@users.noreply.github.com>
2018-11-15 02:02:46 +0200

    AboutBookPage

--- src/views/AboutBookPage.vue
+++ src/views/AboutBookPage.vue
@@ -11,12 +11,12 @@
         <p class="original-book-language"><span class="original-book-language-label">Мова оригіналу: {{ book.language }}</span></p>
         <p class="genre"><span class="genre-label">Жанр: {{ book.genre }}</span></p>
         <p class="book-series"><span class="book-series-label">Серія: {{ book.series }}</span></p>
-        <p class="book-series-number"><span class="book-series-number-label">Номер книги в серії: </span>1</p>
-        <p class="book-year"><span class="book-year-label">Рік: </span>1983</p>
-        <p class="isbn"><span class="isbn-label">ISBN: </span>978-5-699-15629-0</p>
-        <p class="book-language"><span class="book-language-label">Переклад: </span>Російська</p>
+        <p class="book-series-number"><span class="book-series-number-label">Номер книги в серії: </span>{{ book.seriesNumber }}</p>
+        <p class="book-year"><span class="book-year-label">Рік: </span>{{ book.year }}</p>
+        <p class="isbn"><span class="isbn-label">ISBN: </span>{{ book.isbn }}</p>
+        <p class="book-language"><span class="book-language-label">Переклад: </span>{{ book.translate }}</p>
         <p class="book-annotation-label">Короткий опис:</p>
-        <p class="book-annotation-text" id="book-annotation-last-text">Это Великий А’Туин, Вселенская Черепаха, которая бороздит безбрежный космос. Это четыре слона, которые держат на спинах Плоский мир. А это Ринсвинд, самый трусливый волшебник на Диске, и Двацветок, первый турист Плоского мира. Неисчислимые тролли, драконы, волки и Смерть (одна штука) поджидают их в скитаниях по дотоле неведомой нам сказочной вселенной.</p>
+        <p class="book-annotation-text" id="book-annotation-last-text">{{ book.annotation}}</p>
         <div class="taptodwn row no-gutters">
           <div class="col-xl-5 col-lg-6 col-md-8 col-sm-7 col-9">
             <a class=" btn btn-outline-primary btn-block" href="/authors/terry_pratchett/books/the_colour_of_magic/files/the_colour_of_magic.fb2" download role="button">Завантажити</a>
