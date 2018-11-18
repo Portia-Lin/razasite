f6306e89f1e12ad7219d71cf62babc4bb57b082d
escsun <escsunarcher@gmail.com>
2018-11-18 14:33:14 +0200

    Перевел Header на bootstrap компоненты

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="container">
     <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <router-link class="navbar-brand logo" to="/">
+      <b-navbar toggleable="md" variant="light">
+        <b-navbar-brand href="/" class="logo">
           <p>
             <span class="b">B</span><span class="book">ook </span><span class="m">M</span><span class="market">arket</span>
           </p>
-        </router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item ">
-              <router-link to="/" class="nav-link">ГОЛОВНА</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/books" class="nav-link">КНИГИ</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/genres" class="nav-link">ЖАНРИ</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/authors" class="nav-link">АВТОРИ</router-link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-collapse is-nav id="nav_collapse" class="justify-content-end">
+          <b-navbar-nav>
+            <b-nav-item href="/">ГОЛОВНА</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav>
+            <b-nav-item href="/books">КНИГИ</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav>
+            <b-nav-item href="/genres">ЖАНРИ</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav>
+            <b-nav-item href="/authors">АВТОРИ</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </header>
   </div>
 </template>
